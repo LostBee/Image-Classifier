5117d68bf36007965e5f1b3f2595e44cf8e897fd
LostBee <savehoney188@gmail.com>
2025-08-10 11:57:45 +0200

    Adding images which I'm using along with the updated script

--- image_classifier.py
+++ image_classifier.py
@@ -10,18 +10,18 @@ def classify_image(image_path):
     # Let's bring the master chef before we start cooking - The AI Model itself
     # Loading pre-trained model MobileNetV2
     # This model was trained on the ImageNet dataset, which has 1000 categories
-     model = MobileNetV2(weights='imagenet')
-     
+    model = MobileNetV2(weights='imagenet')
+    
     # Load and preprocess the image
     # The model expects images to be exactly 224x224 pixels.
     img = image.load_img(image_path, target_size=(224, 224))
-
+    
     # Convert the image to a format the model understands (a numpy array)
     img_array = image.img_to_array(img)
-
+    
     # Add an extra dimension because the model expects a "batch" of images
     img_array_expanded = np.expand_dims(img_array, axis=0)
-
+    
     # Normalize the image data to match the format the model was trained on
     processed_img = preprocess_input(img_array_expanded)
 
@@ -36,7 +36,7 @@ def classify_image(image_path):
 #Main script
 if __name__ == "__main__":
     # IMPORTANT: Change this to the path of your image file
-    IMAGE_FILE_PATH = 'your_image.jpg' 
+    IMAGE_FILE_PATH = 'image1.jpg' 
 
     try:
         results = classify_image(IMAGE_FILE_PATH)
