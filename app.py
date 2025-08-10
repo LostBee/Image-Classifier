e46ae141c86453464fac9415e13f2d1f17d62e02
LostBee <savehoney188@gmail.com>
2025-08-10 15:57:45 +0200

    Model running on server

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-# Skeleton server
 from flask import Flask
+from tensorflow.keras.applications.resnet50 import ResNet50
 
 app = Flask(__name__)
 
+print("Loading ResNet50 model")
+MODEL = ResNet50(weights="imagenet")     # happens ONE time, right now
+print("Model ready!")
+
 @app.route("/")
 def hello():
-    return "Hello, Flask!"
+    return "Model loaded -server is alive!"
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
